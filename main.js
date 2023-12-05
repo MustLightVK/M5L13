02b7b10d379cc7b99c4bb75b6b6b94232e75ebe3
MustLightVK <mustlightvk@vk.com>
2023-12-05 22:14:12 +0700

    fix

--- main.js
+++ main.js
@@ -1,52 +1,54 @@
 //--------------------------------------task1
-const array1 = [123, 456, 789, 234, 567];
+const array1 = [123, 456, 789, 234, 567, 777, 1069];
 const array2 = [345, 678, 987, 321, 654];
 
 function sumArray (arr1, arr2) {
     let sum1 = 0;
     let sum2 = 0;
-    let sum12 = 0;
-
-    for (let i = 0; i < arr1.length; i++) {
-        sum1 = sum1 + arr1[i];
-        sum2 = sum2 + arr2[i];
-        sum12 = sum1 + sum2;
+    
+    for (let count = 0; count < arr1.length; count++) {
+        sum1 = sum1 + arr1[count];
     }
-    return sum12;
+
+    for (let count = 0; count < arr2.length; count++) {
+        sum2 = sum2 + arr2[count];
+    }
+
+    return sum1 + sum2;
 }
 
 console.log(sumArray(array1, array2));
 //--------------------------------------task2
-let reverse = [1,2,3,4,5];
-reverse = reverse.reverse();
-console.log(reverse);
+let array = [1,2,3,4,5];
+array = array.reverse();
+console.log(array);
 //--------------------------------------task3
 const words = ['радар', 'анна', 'машина', 'шалаш', 'казан', 'варяг', 'потоп'];
 
-function analys(x) {
+function findPalindromes(x) {
     const palindromes = [];
 
-    for (let i = 0; i < x.length; i++) {
-        let reversedWords = x[i].split('').reverse().join('');
+    for (let count = 0; count < x.length; count++) {
+        let reversedWords = x[count].split('').reverse().join('');
         
-        if (x[i] === reversedWords) {
-            palindromes.push(x[i]);
+        if (x[count] === reversedWords) {
+            palindromes.push(x[count]);
         }
     }
 
     return palindromes;
 }
 
-console.log(analys(words)); 
+console.log(findPalindromes(words)); 
 //--------------------------------------task4
 const numbers = [42, 17, -5, 100, 0, 8, -23, -55, -7, 91];
 
 function findMax(arr) {
     let max = arr[0];
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i]; 
+    for (let count = 1; count < arr.length; count++) {
+        if (arr[count] > max) {
+            max = arr[count]; 
         }
     }
     return max;
@@ -54,15 +56,15 @@ function findMax(arr) {
 
 console.log(findMax(numbers)); 
 //--------------------------------------task5
-function multiFunction(number) {
+function splittingNumber(number) {
     let stringNumber = String(number); 
     stringNumber = stringNumber.split('');
     const result = [];
 
-    for (let i = 0; i < stringNumber.length; i++) {
-        result.push(stringNumber[i]); 
+    for (let count = 0; count < stringNumber.length; count++) {
+        result.push(stringNumber[count]); 
 
-        if (stringNumber[i] % 2 !== 0 && stringNumber[i + 1] && stringNumber[i + 1] % 2 !== 0) {
+        if (stringNumber[count] % 2 !== 0 && stringNumber[count + 1] && stringNumber[count + 1] % 2 !== 0) {
             result.push(':');
         }
     }
@@ -70,16 +72,16 @@ function multiFunction(number) {
     return result.join(''); 
 }
 
-console.log(multiFunction(87193201));
+console.log(splittingNumber(87193201));
 //--------------------------------------task6
 const againtNumbers = [1, 2, 3, 4, 5, -1, -2, -3];
 
 function filterNumbers(arr, functionCondition) {
     const filteredNumbers = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        if (functionCondition(arr[i])) {
-            filteredNumbers.push(arr[i]);
+    for (let count = 0; count < arr.length; count++) {
+        if (functionCondition(arr[count])) {
+            filteredNumbers.push(arr[count]);
         }
     }
 
